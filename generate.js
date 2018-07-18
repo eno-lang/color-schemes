63026596e6b183b26a997f500153f508ab10645c
Simon Repp <simon@fdpl.io>
2018-07-18 17:04:48 +0200

    Build color scheme intermediate data, revise html generator and schemes

--- generate.js
+++ generate.js
@@ -1,4 +1,4 @@
-const eno = require('enojs');
+const { parse, EnoDictionary } = require('enojs');
 const fs = require('fs');
 const glob = require('glob');
 const path = require('path');
@@ -6,12 +6,15 @@ const path = require('path');
 const html = require('./templates/html.js');
 
 const generateScheme = filepath => {
-  const name = path.basename(filepath, '.eno');
   const input = fs.readFileSync(filepath, 'utf-8');
-  const document = eno.parse(input);
+  const document = parse(input, { reporter: 'terminal', sourceLabel: filepath });
+
+  const scheme = {
+    name: document.string('name', { required: true })
+  };
 
   const colors = {};
-  for(let colorField of document.dictionary('colors').entries()) {
+  for(let colorField of document.section('colors').elements()) {
     colors[colorField.name] = colorField.color();
   }
 
@@ -22,17 +25,32 @@ const generateScheme = filepath => {
     return colors[value];
   };
 
-  let css = '';
+  const globals = document.section('globals');
 
-  const backgroundColor = document.field('background', colorReference, { required: true });
-  const foregroundColor = document.field('foreground', colorReference, { required: true });
-  css += `body{background-color:${backgroundColor};color:${foregroundColor};}`;
+  scheme.background = globals.field('background', colorReference, { required: true });
+  scheme.caret = globals.field('caret', colorReference, { required: true });
+  scheme.foreground = globals.field('foreground', colorReference, { required: true });
+  scheme.selection = globals.field('selection', colorReference, { required: true });
 
-  for(let scopeField of document.dictionary('scopes').entries()) {
-    css += `.${scopeField.name}{color:${scopeField.value(colorReference, { required: true })};}`;
-  }
+  scheme.scopes = document.section('scopes').elements().map(scopeElement => {
+    let background, foreground, style;
 
-  fs.writeFileSync(path.join(__dirname, `dist/${name}.html`), html(name, css));
+    if(scopeElement instanceof EnoDictionary) {
+      return {
+        background: scopeElement.entry('background', colorReference),
+        foreground: scopeElement.entry('foreground', colorReference, { required: true }),
+        name: scopeElement.name,
+        styles: scopeElement.commaSeparated('styles')
+      };
+    } else {
+      return {
+        foreground: scopeElement.value(colorReference, { required: true }),
+        name: scopeElement.name
+      };
+    }
+  });
+
+  fs.writeFileSync(path.join(__dirname, `dist/html/${scheme.name}.html`), html(scheme));
 }
 
 glob(path.join(__dirname, 'schemes/*.eno'), (err, files) => {
--- templates/html.js
+++ templates/html.js
@@ -1,18 +1,37 @@
-module.exports = (title, css) => `
+module.exports = scheme => `
 <html>
   <head>
     <title>
-      ${title}
+      ${scheme.name}
     </title>
-
-
 
     <style>
       body {
+        background-color: ${scheme.background};
+        color: ${scheme.foreground};
         font-size: 18px;
       }
 
-      ${css}
+      ${scheme.scopes.map(scope => {
+        result = `.${scope.name}{`;
+        result += `color:${scope.foreground};`;
+
+        if(scope.background)
+          result += `background-color:${scope.background};`;
+
+        if(scope.styles) {
+          for(let style of scope.styles) {
+            if(style === 'bold')
+              result += `font-weight:bold;`;
+            else if(style === 'italic')
+              result += `font-style:italic;`;
+          }
+        }
+
+        result += '}';
+
+        return result;
+      }).join('\n')}
     </style>
   </head>
   <body>
@@ -32,22 +51,22 @@ something
 <span class="name">blitz</span> <span class="copy_operator">&lt;</span> <span class="template">blotz</span>
 
 <span class="name">Telephone Numbers</span><span class="name_operator">:</span>
-<span class="entry">Ben</span> = +49 1943 24724784
-<span class="entry">Rachel</span> = +59 3459 35935593
-<span class="escape_operator">\`</span><span class="entry">Dave</span><span class="escape_operator">\`</span> = +69 9025 93593531
+<span class="entry">Ben</span> <span class="entry_operator">=</span> +49 1943 24724784
+<span class="entry">Rachel</span> <span class="entry_operator">=</span> +59 3459 35935593
+<span class="escape_operator">\`</span><span class="entry">Dave</span><span class="escape_operator">\`</span> <span class="entry_operator">=</span> +69 9025 93593531
 
-<span class="section"># Body</span>
-<span class="section"># Body < Foo</span>
-<span class="section">## <span class="escape_operator">\`</span>Limbs<span class="escape_operator">\`</span></span>
-<span class="section">### Four</span>
-<span class="section">#### 4</span>
-<span class="section">##### 5</span>
-<span class="section">###### 6</span>
-<span class="section">###### 7</span>
-<span class="section">### Left Arm</span>
+<span class="section1_operator">#</span> <span class="section1_name">Body</span>
+<span class="section1_operator">#</span> <span class="section1_name">Body</span> <span class="section1_copy_operator">&lt;</span> <span class="section1_template">Foo</span>
+<span class="section2_operator">##</span> <span class="section2_escape_operator">\`</span><span class="section2_name">Limbs</span><span class="section2_escape_operator">\`</span></span>
+<span class="section3_operator">###</span> <span class="section3_name">Four</span>
+<span class="section4_operator">####</span> <span class="section4_name">4</span>
+<span class="section5_operator">#####</span> <span class="section5_name">5</span>
+<span class="section6_operator">######</span> <span class="section6_name">6</span>
+<span class="section6_operator">#######</span> <span class="section6_name">7</span>
+<span class="section3_operator">###</span> <span class="section3_name">Left Arm</span>
 
-<span class="section">### <span class="escape_operator">\`</span>Right Arm<span class="escape_operator">\`</span></span> <span class="copy_operator">&lt;</span> <span class="template">Left Arm</span>
-<span class="section">### <span class="escape_operator">\`</span>Right Arm<span class="escape_operator">\`</span></span> <span class="copy_operator">&lt;&lt;</span> <span class="template">Left Arm</span>
+<span class="section3_operator">###</span> <span class="section3_escape_operator">\`</span><span class="section3_name">Right Arm</span><span class="section3_escape_operator">\`</span></span> <span class="copy_operator">&lt;</span> <span class="template">Left Arm</span>
+<span class="section3_operator">###</span> <span class="section3_escape_operator">\`</span><span class="section3_name">Right Arm</span><span class="section3_escape_operator">\`</span></span> <span class="copy_operator">&lt;&lt;</span> <span class="template">Left Arm</span>
 
 <span class="escape_operator">\`</span><span class="name">--format pretty</span><span class="escape_operator">\`</span><span class="name_operator">:</span> Pretty formatting option
 
@@ -56,8 +75,8 @@ something
 <span class="item_operator">-</span> Frankenstein
 
 <span class="escape_operator">\`\`</span><span class="name">Use \`\${arg}\` style options</span><span class="escape_operator">\`\`</span><span class="name_operator">:</span>
-<span class="entry">'--output dir'</span> = Specify a different output directory
-<span class="entry">'--filters disabled'</span> = Enable/disable filters
+<span class="entry">'--output dir'</span> <span class="entry_operator">=</span> Specify a different output directory
+<span class="entry">'--filters disabled'</span> <span class="entry_operator">=</span> Enable/disable filters
 
 <span class="invalid">asdfa</span>
 
